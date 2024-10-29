ad449a8baf8dd31ba80874f4fdc08045ea1fe858
kimdahee7 <kikidahee7@duksung.ac.kr>
2024-10-29 18:26:44 +0900

    [refactor] LoginData 변수명 변경

--- app/src/main/java/com/example/pill_mate_android/ui/login/LoginData.kt
+++ app/src/main/java/com/example/pill_mate_android/ui/login/LoginData.kt
@@ -1,6 +1,5 @@
 package com.example.pill_mate_android.ui.login
 
 data class LoginData(
-    val accessToken: String?,
-    var alarmMarketing: Boolean?
+    val kakaoAccessToken: String?, var marketingAlarm: Boolean?
 )
