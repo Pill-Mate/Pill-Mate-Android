700224ba63911e41e84facdd158f732e93eb14f7
kimdahee7 <kikidahee7@duksung.ac.kr>
2025-01-12 17:49:56 +0900

    [feat] #5 - MainActivity 상태바 색상 변경

--- app/src/main/java/com/example/pill_mate_android/ui/main/activity/MainActivity.kt
+++ app/src/main/java/com/example/pill_mate_android/ui/main/activity/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.pill_mate_android.ui.main.activity
 
+import android.os.Build.VERSION_CODES
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.Fragment
 import com.example.pill_mate_android.R.id
 import com.example.pill_mate_android.databinding.ActivityMainBinding
@@ -16,11 +19,13 @@ class MainActivity : AppCompatActivity(), MainContract.View {
     private lateinit var binding: ActivityMainBinding
     private lateinit var presenter: MainPresenter
 
+    @RequiresApi(VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
         setContentView(binding.root)
+
+        setDefaultStatusBar()
 
         presenter = MainPresenter(this)
         presenter.onCreate()
@@ -44,4 +49,21 @@ class MainActivity : AppCompatActivity(), MainContract.View {
     override fun showFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().replace(id.frm_main, fragment).commitAllowingStateLoss()
     }
+
+    // 기본 상태바로 설정
+    fun setDefaultStatusBar() {
+        window.apply {
+            statusBarColor = ContextCompat.getColor(this@MainActivity, android.R.color.transparent) // 기본 상태바 색상
+            WindowInsetsControllerCompat(this, decorView).isAppearanceLightStatusBars = true // 기본 상태바 아이콘(검정)
+        }
+    }
+
+    // 상태바 색상 변경
+    fun setStatusBarColor(colorResId: Int, isLightStatusBar: Boolean) {
+        window.apply {
+            statusBarColor = ContextCompat.getColor(this@MainActivity, colorResId) // 상태바 색상 변경
+            WindowInsetsControllerCompat(this, decorView).isAppearanceLightStatusBars = isLightStatusBar // 아이콘 색상 변경
+        }
+    }
+
 }
