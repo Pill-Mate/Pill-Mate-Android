c7c39b0ff5d18610ae72551aeda2b81f5f565bf1
kimdahee7 <kikidahee7@duksung.ac.kr>
2025-01-20 23:59:33 +0900

    [feat] FCM firebase 연결

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -73,5 +74,8 @@ dependencies {
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    debugImplementation(libs.androidx.ui.test.manifest) // Firebase BoM
+    //implementation(platform("com.google.firebase:firebase-bom:33.8.0")) -> 기존 버전(다운그레이드한 상태)
+    implementation(platform("com.google.firebase:firebase-bom:32.2.3"))
+    implementation("com.google.firebase:firebase-analytics")
 }
