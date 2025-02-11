25be6eed693426215c44450999d0c9ea0ce17309
kimdahee7 <kikidahee7@duksung.ac.kr>
2025-02-12 00:44:03 +0900

    [feat] #9 - 약물관리 TabLayout 생성

---
+++ app/src/main/java/com/pill_mate/pill_mate_android/ui/pilledit/InactiveMedicineFragment.kt
@@ -0,0 +1,21 @@
+package com.pill_mate.pill_mate_android.ui.pilledit
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.pill_mate.pill_mate_android.R
+
+class InactiveMedicineFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_inactive_medicine, container, false)
+    }
+}
