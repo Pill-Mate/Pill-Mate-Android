33c807393a3a000c76081a02c77dde5d771a7da7
kimdahee7 <kikidahee7@duksung.ac.kr>
2025-04-12 12:57:27 +0900

    [feat] #8 - 루틴 변경 다이얼로그 추가

--- app/src/main/java/com/pill_mate/pill_mate_android/ui/setting/ConfirmDialogInterface.kt
+++ app/src/main/java/com/pill_mate/pill_mate_android/ui/setting/ConfirmDialogInterface.kt
@@ -3,4 +3,5 @@ package com.pill_mate.pill_mate_android.ui.setting
 interface ConfirmDialogInterface {
     fun onSignOutButtonClick()
     fun onLogOutButtonClick()
+    fun onSettingRoutineButtonClick()
 }
