3dce4822cf7cfeb8efa8345bf30989fc0479f065
kimdahee7 <kikidahee7@duksung.ac.kr>
2025-05-17 16:21:44 +0900

    [release] 1.1.0 QA 최종 반영

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.pill_mate.pill_mate_android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10001
+        versionCode = 10002
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
