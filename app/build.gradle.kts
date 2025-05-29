04cf011a4c50c713fdc3bcf606e5ff30e595e2de
kimdahee7 <kikidahee7@duksung.ac.kr>
2025-05-30 01:38:54 +0900

    [release] 버전 코드 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.pill_mate.pill_mate_android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
