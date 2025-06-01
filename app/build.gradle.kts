38fe712bafd8c8170576b05e62263e6d1a9d38fd
kimdahee7 <kikidahee7@duksung.ac.kr>
2025-06-02 08:18:10 +0900

    [release] 1.2.1 배포 버전

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.pill_mate.pill_mate_android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.2.0"
+        versionCode = 6
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
