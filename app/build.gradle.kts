5d942f6a1327b38cc6408c68c940dc87aee2c186
kimdahee7 <kikidahee7@duksung.ac.kr>
2025-09-19 01:01:04 +0900

    [chore] Material 1.12.0 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -132,7 +132,7 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.15.1") // Glide
     kapt("com.github.bumptech.glide:compiler:4.15.1")
     implementation("de.hdodenhof:circleimageview:3.1.0")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.security:security-crypto:1.1.0-alpha06") //EncryptedSharedPreferences
     implementation(platform("com.google.firebase:firebase-bom:33.12.0")) // Import the Firebase BoM
     implementation("com.google.firebase:firebase-analytics")
