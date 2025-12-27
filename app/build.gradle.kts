5c7c2b9cc7ec1f53877e5024c90a1ad0bfc7376a
kimdahee7 <kikidahee7@duksung.ac.kr>
2025-12-27 16:57:49 +0900

    [chore] 테스트 빌드 버전 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.pill_mate.pill_mate_android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.3.0"
+        versionCode = 10009
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
