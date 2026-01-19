1bb2951d3bad4885a5b0ae28698d618f541cc0ad
kimdahee7 <kikidahee7@duksung.ac.kr>
2026-01-19 21:41:15 +0900

    [chore] 배포 빌드 오류 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.pill_mate.pill_mate_android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 11
+        versionCode = 12
         versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
