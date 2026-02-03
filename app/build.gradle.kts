45c6abfa01e5db39cdc1bc11558d166307ead198
kimdahee7 <kikidahee7@duksung.ac.kr>
2026-02-04 00:39:12 +0900

    [chore] 릴리즈 빌드 버전 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.pill_mate.pill_mate_android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.4.1"
+        versionCode = 12
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
