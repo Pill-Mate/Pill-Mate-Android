e69ad6ca55ef0cf53f5d4480c5fa866d253413c0
Kimdahee <kikidahee7@duksung.ac.kr>
2026-09-06 16:25:57 +0900

    [release] 1.4.4 QA 반영 버전

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.pill_mate.pill_mate_android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10012
-        versionName = "1.4.3"
+        versionCode = 10013
+        versionName = "1.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
